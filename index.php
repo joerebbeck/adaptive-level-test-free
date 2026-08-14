520bb5b9fa3dfd5d5a7f04bb93448d17403c458b
joerebbeck <joerebbeck@hotmail.com>
2026-08-14 21:56:11 +0200

    Add Go Pro action link to Plugins page

--- index.php
+++ index.php
@@ -188,7 +188,8 @@ add_action( 'wp_enqueue_scripts', 'adaptive_test_enqueue_scripts' );
 
 function adaptive_test_plugin_action_links( $links ) {
     $settings_link = '<a href="' . esc_url( admin_url( 'options-general.php?page=adaptive-level-test' ) ) . '">' . esc_html__( 'Settings', 'adaptive-level-test' ) . '</a>';
-    array_unshift( $links, $settings_link );
+    $upgrade_link  = '<a href="https://idiomiq.com/adaptive-level-test/#upgrade" target="_blank" rel="noopener" style="color:#f59e0b;font-weight:600;">' . esc_html__( 'Go Pro', 'adaptive-level-test' ) . '</a>';
+    array_unshift( $links, $upgrade_link, $settings_link );
     return $links;
 }
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'adaptive_test_plugin_action_links' );
